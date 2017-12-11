d804245551ff3701cd194327fed3a499b9be32a3
andrievskaja <31058303+andrievskaja@users.noreply.github.com>
2017-12-11 15:45:16 +0200

    Update Role.java

--- business-model/src/main/java/com/andrievskaja/business/model/Role.java
+++ business-model/src/main/java/com/andrievskaja/business/model/Role.java
@@ -6,10 +6,7 @@
 
 package com.andrievskaja.business.model;
 
-/**
- *
- * @author finger
- */
+
 public final class Role {
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final String ROLE_USER = "ROLE_USER";
